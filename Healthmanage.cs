3dc7d8381853835f2a80c1fa39e15dc23ee8f0e6
NguyenTienDat <khaidaikhai222@gmail.com>
2025-10-31 10:37:40 +0700

    hgi

---
+++ Healthmanage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //làm việc với đối tượng Image
+using TMPro; //Dùng để hiển thị chữ bằng TextMeshPro (rõ và đẹp hơn Text thường).
+
+public class Healthmanage : MonoBehaviour
+{
+    public Image fillBar; //ảnh thanh máu
+    public TextMeshProUGUI valueText; //chữ hiển thị số máu
+
+
+    public void UpdateBar(int currentHealth, int maxHealth) //cập nhật thanh máu và chữ hiển thị mỗi khi nhân vật bị thương hoặc hồi máu
+    {
+        fillBar.fillAmount = (float)currentHealth / (float)maxHealth;  //thể hiện phần trăm máu  
+        valueText.text = currentHealth.ToString() + " / " + maxHealth.ToString(); //hiện số máu hiện tại và máu tối đa
+    }
+}
---
+++ healthPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int currentHealth; //lượng máu hiện tại
+    public int maxHealth; //lượng máu tối đa
+    public Healthmanage healthmanage; //lấy cái Healthmanage để cập nhật thanh máu và chữ hiển thị của người chơi
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthmanage.UpdateBar(currentHealth, maxHealth); //cập nhật thanh máu và chữ hiển thị lúc bắt đầu
+    }
+
+    public void TakeDamage(int damage) //hàm nhận sát thương
+    {
+        currentHealth -= damage; //trừ máu hiện tại đi sát thương nhận vào
+
+        healthmanage.UpdateBar(currentHealth, maxHealth); //cập nhật thanh máu và chữ hiển thị sau khi nhận sát thương
+
+        if (currentHealth <= 0)
+            Debug.Log("player chết rồi!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Trap"))
+            TakeDamage(20);
+        else if (collision.CompareTag("Enemy"))
+            TakeDamage(20);
+    }
+
+}
