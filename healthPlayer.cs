43dfc3dff210527a67a2c9e34eddca89dc2e20f2
NguyenTienDat <khaidaikhai222@gmail.com>
2025-11-01 10:10:42 +0700

    giehgi

--- healthPlayer.cs
+++ healthPlayer.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     int currentHealth; //lượng máu hiện tại
     public int maxHealth; //lượng máu tối đa
     public Healthmanage healthmanage; //lấy cái Healthmanage để cập nhật thanh máu và chữ hiển thị của người chơi
+    public UnityEvent Detroyself; //sự kiện hủy đối tượng
 
 
     void Start()
     {
         currentHealth = maxHealth;
         healthmanage.UpdateBar(currentHealth, maxHealth); //cập nhật thanh máu và chữ hiển thị lúc bắt đầu
+    }
+
+    private void death()
+    {
+        Detroyself.AddListener(Death);
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject); //hủy đối tượng
     }
 
     public void TakeDamage(int damage) //hàm nhận sát thương
@@ -22,7 +34,11 @@ public class NewBehaviourScript : MonoBehaviour
         healthmanage.UpdateBar(currentHealth, maxHealth); //cập nhật thanh máu và chữ hiển thị sau khi nhận sát thương
 
         if (currentHealth <= 0)
-            Debug.Log("player chết rồi!");
+        {
+            Detroyself.Invoke(); //nếu máu hiện tại nhỏ hơn hoặc bằng 0 thì gọi hàm hủy đối tượng
+        }
+
+        healthmanage.UpdateBar(currentHealth, maxHealth);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
